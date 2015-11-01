7af3be1e69de7a8b2dff30b36728909b6927cd04
Mahfuzur Rahman <mahfuz@student.sust.edu>
2015-11-02 01:57:25 +0600

    12646 - Zero or One

---
+++ 12646 - Zero or One.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    while(scanf("%d %d %d", &a, &b, &c) != EOF) {
+        if(a == b && b == c)
+            printf("*\n");
+        else if(b == c)
+            printf("A\n");
+        else if(c == a)
+            printf("B\n");
+        else if(a == b)
+            printf("C\n");
+    }
+    return 0;
+}
+
